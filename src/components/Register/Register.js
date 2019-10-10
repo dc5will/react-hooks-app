23eb21dfc7a9ff6f0cc108930df4f4affe635496
William Y Wong <wwong1987@gmail.com>
2019-10-10 16:27:39 -0700

    reworked registration page to successfully authenticate with firebase

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,28 +1,64 @@
 import React, { useState } from "react";
+import Navbar from "../Navbar/Navbar";
+import firebase from "../../firebase";
 
 export default function Register(props) {
-  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = e => {
-    // fetch request here
-    e.preventDefault();
-    props.storeUser({ username, password });
-  };
+  async function onRegister() {
+    try {
+      await firebase.register(name, email, password);
+      props.history.replace("/dashboard");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        onchange={e => setUsername(e.target.value)}
-        placeholder="enter username"
-      />
-      <input
-        type="text"
-        onchange={e => setPassword(e.target.value)}
-        placeholder="enter password"
-      />
-      <input type="submit" />
-    </form>
+    <main>
+      <Navbar {...props} />
+      <div className="container">
+        <h2>Register</h2>
+        <form onSubmit={e => e.preventDefault() && false}>
+          <div className="input-field">
+            <input
+              placeholder="username"
+              type="text"
+              value={name}
+              className="validate"
+              aria-label="username"
+              onChange={e => setName(e.target.value)}
+            />
+          </div>
+          <div className="input-field">
+            <input
+              placeholder="email"
+              type="text"
+              value={email}
+              className="validate"
+              aria-label="email"
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="input-field">
+            <input
+              placeholder="password"
+              type="password"
+              value={password}
+              className="validate"
+              aria-label="password"
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="btn-large" onClick={onRegister}>
+            Register
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
